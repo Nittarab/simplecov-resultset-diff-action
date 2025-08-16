d905fa4528afda154cb3c41ec9432dbeccadd420
Nittarab <p.barattin@gmail.com>
2025-08-16 20:20:04 +0200

    fixes

--- src/utils.ts
+++ src/utils.ts
@@ -111,7 +111,7 @@ function formatCoverageStatsDiff(
   // Show change in covered/total if there's a difference
   if (coveredDiff !== 0 || totalDiff !== 0) {
     const coveredSign = coveredDiff > 0 ? '+' : ''
-    const totalSign = totalDiff > 0 ? '+' : totalDiff === 0 ? '' : ''
+    const totalSign = totalDiff > 0 ? '+' : ''
     const totalDiffStr = totalDiff === 0 ? '0' : `${totalSign}${totalDiff}`
     result = `${coveredSign}${coveredDiff}/${totalDiffStr} `
   }
